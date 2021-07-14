40fb45238ebb4e15be1957157afaabe002047a31
Reval Fahmi <reval.fahmi42@gmail.com>
2021-07-14 11:23:03 +0700

    E-Commerce CodingStudio

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Category;
+use App\Product;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call(CategorySeeder::class);
+        factory(Product::class, 50)->create();
+    }
+}
